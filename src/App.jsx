f5a473dbaf27138569e87d6c7e1372feb9311faa
developerMasum <developer.masum2001@gmail.com>
2023-03-30 21:27:54 +0600

    vite installed

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <h2>Hi</h2>
+      
+    </div>
+  )
+}
+
+export default App
